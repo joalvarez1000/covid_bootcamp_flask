405de96087687311ea65dba4891453129b103df0
DESKTOP-LUKPNTP\josea <jose.alvarec@mail.udp.cl>
2021-05-16 13:15:50 -0400

    creamos rutas para el localhost

--- covid/views.py
+++ covid/views.py
@@ -1,11 +1,48 @@
 '''
 creamos una ruta para los datos que estan en el modulo covid
-aqui dejaremos las rutas
+aqui dejaremos las rutas para el navegador
 '''
 
 from covid import app   
+import csv
+import json
 
-@app.route('/')
+@app.route('/provincias')
+def provincias (): 
+    fichero = open ('data/provincias.csv', 'r') #abrimos el fichero provincias
+    csvreader = csv.reader (fichero, delimiter =',') #ocupamos el metodo csv para delimitar la estructura ','
 
-def index (): #puedo colocar el nombre que yo quiera
-    return 'Flask esta funcionando desde views!'
+    lista = []
+    for registro in csvreader:
+        d = {'codigo':registro[0], 'provincia':registro[1]}
+        lista.append (d)
+
+    fichero.close()
+    print(lista)
+    return json.dumps(lista)
+
+@app.route ('/provincia/<codigoProvincia>') #cuando se quiere ingresar un parametro y no un literal <>
+def laprovincia(codigoProvincia): # dentro de la funcion debe ir el mismo nombre dentro de <>
+    fichero = open ('data/provincias.csv', 'r')
+    dictreader = csv.DictReader (fichero, fieldnames=['codigo','provincia'])
+    for registro in dictreader:
+        if registro ['codigo'] == codigoProvincia:
+            return registro['provincia']
+
+    fichero.close()
+    return 'el valor no existe. largo de aqui!'
+
+
+'''
+@app.route ('/casos/<int:years>' defaults= {'mes'= None, 'dia'= None}) # Solo para que devuelva los valores del año
+@app.route ('/casos/<int:years>/<int:mes>'defaults= {'dia'= None}) #Solo devuelve los valores año mes
+'''
+
+@app.route ('/casos/<int:years>/<int:mes>/<int:dia>')
+def casos (years, mes, dia):
+    pass
+
+'''
+1er caso: devolver el numero total de casos covid en un dia del año determinado para todas las provincias
+2do caso . lo mismo pero detallado por tipo. PCR, AC, AG, ELISA, DESCONOCIDO - OTR
+'''
