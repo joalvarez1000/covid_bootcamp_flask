df62f6bb86905d39296954b674967e603ec7c84f
DESKTOP-LUKPNTP\josea <jose.alvarec@mail.udp.cl>
2021-05-21 20:23:34 -0400

    inicio de plantillas

--- covid/views.py
+++ covid/views.py
@@ -2,7 +2,7 @@
 creamos una ruta para los datos que estan en el modulo covid
 aqui dejaremos las rutas para el navegador
 '''
-
+from flask import render_template, request
 from covid import app   
 import csv
 import json
@@ -32,17 +32,68 @@ def laprovincia(codigoProvincia): # dentro de la funcion debe ir el mismo nombre
     fichero.close()
     return 'el valor no existe. largo de aqui!'
 
+@app.route ('/casos/<int:year>', defaults  = {'mes' : None, 'dia': None}) # Solo para que devuelva los valores del año
+@app.route ('/casos/<int:year>/<int:mes>') #Solo devuelve los valores año mes
+@app.route ('/casos/<int:year>/<int:mes>/<int:dia>')
+def casos (year, mes, dia = None):
+    
+    
+    if not mes:
+        fecha = "{:04d}".format(year)
+    elif not dia:
+        fecha = "{:04d}-{:02d}".format(year,mes)
+    else:
+        fecha = "{:04d}-{:02d}-{:02d}".format(year,mes,dia)
+    
+    fichero = open ('data/casos_diagnostico_provincia.csv', 'r')
+    dictreader = csv.DictReader (fichero)
 
-'''
-@app.route ('/casos/<int:years>' defaults= {'mes'= None, 'dia'= None}) # Solo para que devuelva los valores del año
-@app.route ('/casos/<int:years>/<int:mes>'defaults= {'dia'= None}) #Solo devuelve los valores año mes
-'''
+    res = {
+        'num_casos' : 0,
+        'num_casos_prueba_pcr' : 0,
+        'num_casos_prueba_test_ac' : 0,
+        'num_casos_prueba_ag' :0,
+        'num_casos_prueba_elisa' : 0,
+        'num_casos_prueba_desconocida' : 0
+    }
 
-@app.route ('/casos/<int:years>/<int:mes>/<int:dia>')
-def casos (years, mes, dia):
-    pass
+    
+    for registro in dictreader:
+        if fecha in registro ['fecha']:
+            for clave in res:
+                res[clave] += int (registro[clave])
 
+        elif registro['fecha'] > fecha:
+            break
+        
+    fichero.close()
+    return json.dumps(res)
+            
 '''
 1er caso: devolver el numero total de casos covid en un dia del año determinado para todas las provincias
 2do caso . lo mismo pero detallado por tipo. PCR, AC, AG, ELISA, DESCONOCIDO - OTR
 '''
+
+@app.route ('/incidenciasdiarias', methods =['GET', 'POST'])
+def incidencia():
+    if request.method == 'GET':
+        return render_template("alta.html", casos_pcr=0)
+    
+    #Validar que los valores de los casos sean números y sean enteros positivos
+    #estudiar objeto request de la libreria
+    Valores = request.form
+    #vslorar num_casos_prueba_pcr >= 0 y entero
+    try:
+        num_pcr = int(request.form["num_casos_prueba_pcr"])
+        if num_pcr < 0:
+            raise ValueError('Debe ser positivo')
+    except ValueError:
+        return render_template("alta.html", casos_pcr="Introduce un valor correcto")
+    
+    #Validar la información que llega
+    #Que el total de casos sea la suma del resto de casos
+    #Que la provincia sea correcta
+    #Que la fecha sea correcta en formato y supongo que en valor
+    #Que la fecha no sea a futuro y la fecha no sea anterior a fecha covid
+    #Si la informacion es incorrecta
+    return "Se ha hecho un post"
